b74bc1245d05efc96eec34c8acc7090497b15f86
12Me21 <12Me21.mc@gmail.com>
2022-09-17 19:50:52 -0400

    FIRST TRY, EEEEIDIOT!

--- randomous.js
+++ randomous.js
@@ -50,85 +50,41 @@ class Grp extends CanvasRenderingContext2D {
 		let old = pixels[x+y*width]
 		let col = Color.int32(this.fillStyle)
 		
-		let queue = [x, y]
-		let check = (x, y, push)=>{
-			if (x<0 || y<0 || x>=width || y>=height)
-				return false
-			if (pixels[x+y*width]!=old)
-				return false
-			pixels[x+y*width] = col
-			if (push)
-				queue.push([x, y])
-			return true
+		let scan = (x, dir, end, y)=>{
+			//&& Atomics.compareExchange(pixels, x+dir+y*width, old, col)==old
+			while (x!=end && pixels[x+dir + y*width]==old) {
+				pixels[x+dir + y*width] = col
+				x += dir
+			}
+			return x
 		}
 		
-		if (!check(x,y))
-			return
-		
-		let check3 = (x, y)=>{
-			check(x, y+1, true)
-			check(x, y-1, true)
+		let queue = [[x+1, x-1, y, -1]]
+		let find_spans = (left, right, y, dy)=>{
+			y+=dy
+			if (y<0 || y>=height)
+				return
+			for (let x=left; x<=right; x++) {
+				let stop = scan(x-1, +1, right, y)
+				if (stop>=x) {
+					queue.push([x, stop, y, dy])
+					x = stop
+				}
+			}
 		}
-		//opt to do:
-		// say we're scanning upwards from prev line (i.e. from a coordinate pushed by queue.push(x,y-1))
-		// we only need to scan downwards if we are past the original extent
-		// ex:
-		// ##!!!!!!!!!!!!!!!!!!!!!!!
-		// ##***********************#
-		// ##!!!!!!###ffffffffffffff#
-		// where "f" is the pixels filled by that previous row
-		// and * is the extent of the current row
-		// we only need to check pixels marked with "!"
-		
-		// SO: `f` should push a SPAN, not individual pixels:
-		
-		// todo: -should this span be extended before pushed to stack?
-		
-		// Each span has:
-		// - y coordinate
-		// - start x (`[`)
-		// - end x (`]`)
-		// - direction (up `^` or down `v`)
-		```
-		// 1: pop a span from the stack:
-		// ██████        ██      ███
-		// ███       [^^^^^^^]     █
-		// █      ███▓▓▓▓▓▓▓▓▓██████
-		
-		// 2: scan left and right, to extend the span:
-		// ██████        ██      ███
-		// ██◙◌◌◌◌◌◌◌[^^^^^^^]◌◌◌◌◌◙
-		// █      ███▓▓▓▓▓▓▓▓▓██████
-		
-		// 3: fill that area:
-		// ██████        ██      ███
-		// ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
-		// █      ███▓▓▓▓▓▓▓▓▓██████
-		
-		// 4: scan the rows above and below:
-		// ███◙◙◙◌◌◌◌◌◌◌◌◙◙◌◌◌◌◌◌◙◙█
-		// ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
-		// █  ◌◌◌◌◙◙◙▓▓▓▓▓▓▓▓▓◙◙◙◙◙█
-		// Optimization: don't check the area
-      // directly below the original span
-      // (or above, if it was 'facing' downwards)
-      // since we know that's already been filled
-      // by whoever pushed that span to the stack
-
-		// identify spans. push them to the stack:
-		// ██████[^^^^^^]██[^^^^]███
-		// ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
-		// █  [vv]███▓▓▓▓▓▓▓▓▓██████
-		```
 		
 		while (queue.length) {
-			let [x, y] = queue.pop()
-			check3(x, y)
-			for (let s = x-1; check(s, y); s--)
-				check3(s, y)
-			for (let s = x+1; check(s, y); s++)
-				check3(s, y)
+			let [x1, x2, y, dy] = queue.pop()
+			// expand current span
+			let left = scan(x1, -1, 0, y)
+			let right = scan(x2, +1, width, y)
+			// "forward"
+			find_spans(left, right, y, dy)
+			// "backward"
+			find_spans(left, x1-1, y, -dy)
+			find_spans(x2+1, right, y, -dy)
 		}
+		
 		this.put_pixels(pixels)
 	}
 	replace_color(original) {
